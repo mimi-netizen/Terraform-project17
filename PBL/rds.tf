3dd4e05cfc15232ed9073b71b1e19a88e0440b26
Celyne <dianakydd2@gmail.com>
2024-08-25 22:37:39 +0300

    Initial commit

---
+++ PBL/rds.tf
@@ -0,0 +1,28 @@
+# This section will create the subnet group for the RDS  instance using the private subnet
+resource "aws_db_subnet_group" "kydd-rds" {
+  name       = "kydd-rds"
+  subnet_ids = [aws_subnet.private[2].id, aws_subnet.private[3].id]
+
+ tags = merge(
+    var.tags,
+    {
+      Name = "kydd-rds"
+    },
+  )
+}
+
+# create the RDS instance with the subnets group
+resource "aws_db_instance" "kydd-rds" {
+  allocated_storage      = 20
+  storage_type           = "gp2"
+  engine                 = "mysql"
+  engine_version         = "5.7"
+  instance_class         = "db.t2.micro"
+  username               = var.master-username
+  password               = var.master-password
+  parameter_group_name   = "default.mysql5.7"
+  db_subnet_group_name   = aws_db_subnet_group.kydd-rds.name
+  skip_final_snapshot    = true
+  vpc_security_group_ids = [aws_security_group.datalayer-sg.id]
+  multi_az               = "true"
+}
---
+++ PBL/variables.tf
@@ -0,0 +1,76 @@
+variable "region" {
+      default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+    default = "172.16.0.0/16"
+}
+
+variable "enable_dns_support" {
+    default = "true"
+}
+
+variable "enable_dns_hostnames" {
+    default ="true" 
+}
+
+variable "enable_classiclink" {
+    default = "false"
+}
+
+variable "enable_classiclink_dns_support" {
+    default = "false"
+}
+
+variable "preferred_number_of_public_subnets" {
+      default = null
+}
+
+variable "preferred_number_of_private_subnets" {
+      default = null
+}
+
+variable "name" {
+  type    = string
+  default = "Project-17"
+
+}
+
+variable "tags" {
+  description = "A mapping of tags to assign to all resources."
+  type        = map(string)
+  default     = {}
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment"
+
+}
+
+variable "ami" {
+  type        = string
+  description = "AMI ID for the launch template"
+}
+
+
+variable "keypair" {
+  type        = string
+  description = "key pair for the instances"
+}
+
+variable "account_no" {
+  type        = number
+  description = "the aws account number"
+}
+
+
+variable "master-username" {
+  type        = string
+  description = "RDS admin username"
+}
+
+variable "master-password" {
+  type        = string
+  description = "RDS master password"
+}
